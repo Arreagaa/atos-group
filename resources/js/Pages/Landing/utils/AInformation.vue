ac5793cc8eeb104e724f5849fc85cf5ebb1c181e
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2023-03-28 16:07:40 -0600

    fix

--- resources/js/Pages/Landing/utils/AInformation.vue
+++ resources/js/Pages/Landing/utils/AInformation.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="boldFont lg:block hidden container max-w-fit mx-auto mt-4">
-        <div class="flex text-start justify-around w-full gap-20 uppercase">
+        <div class="flex text-start justify-around w-full uppercase">
             <h1 class="font-bold tracking-tight text-zinc-600 mt-5 mt-32">
                 <span class="block text-zinc-600 text-2xl"
                     >Actualmente producimos</span
