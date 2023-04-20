02602e65fd40c2700acd36c4b38fa3460d21b4d2
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2023-04-20 12:38:47 -0600

    fix

--- resources/js/Pages/Landing/AHero.vue
+++ resources/js/Pages/Landing/AHero.vue
@@ -1,22 +1,31 @@
+<script></script>
 <template>
-    <section id="hero">
+    <section
+        id="hero"
+        class="w-full h-screen overflow-hidden relative md:h-screen-150vh md:aspect-w-100 md:aspect-h-150"
+    >
         <div class="hero-container">
             <path d="M51,267 C243,25 254,26 459,283" />
             <div id="heroCarousel" class="carousel slide carousel-fade">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <div
-                            class="lg:block hidden carousel-container absolute top-40 left-52"
+                            class="carousel-container absolute top-40 lg:left-52 left-4"
                         >
                             <section
-                                class="flex flex-wrap items-center -mx-6 px-4 py-4 mx-auto w-full lg:max-w-screen-lg sm:max-w-screen-sm md:max-w-screen-md"
+                                class="flex flex-wrap items-center lg:-mx-6 lg:px-4 lg:py-4 -mx-3 px-3 mx-auto w-full lg:max-w-screen-lg sm:max-w-screen-sm md:max-w-screen-md"
                             >
-                                <div class="flex px-3 w-full">
+                                <div class="flex lg:px-3 px-1 w-full">
                                     <div
                                         class="text-amber-300 mx-auto mb-8 max-w-lg text-center lg:mx-0 lg:max-w-md lg:text-left"
                                     >
                                         <h2
-                                            class="blackFont atos mb-4 font-semibold text-left lg:text-8xl"
+                                            class="blackFont atos mb-4 font-semibold text-left lg:text-8xl max-[375px]:text-6xl"
+                                            :class="
+                                                $i18n.locale === 'es'
+                                                    ? 'text-7xl'
+                                                    : 'text-6xl'
+                                            "
                                         >
                                             {{
                                                 $t(
@@ -26,41 +35,7 @@
                                         </h2>
 
                                         <p
-                                            class="lightFont visible mx-0 mt-3 mb-0 text-2xl leading-relaxed text-left text-slate-200 pr-8"
-                                        >
-                                            {{
-                                                $t(
-                                                    "450 hectáreas con producción de banano de la variedad Gran Enano."
-                                                )
-                                            }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </section>
-                        </div>
-
-                        <div
-                            class="lg:hidden carousel-container absolute top-40 left-4"
-                        >
-                            <section
-                                class="flex flex-wrap items-center -mx-3 px-3 mx-auto w-full lg:max-w-screen-lg sm:max-w-screen-sm md:max-w-screen-md"
-                            >
-                                <div class="flex px-1 w-full">
-                                    <div
-                                        class="text-amber-300 mx-auto mb-8 max-w-lg text-center lg:mx-0 lg:max-w-md lg:text-left"
-                                    >
-                                        <h2
-                                            class="blackFont atos mb-4 text-7xl font-semibold text-left max-[375px]:text-6xl"
-                                        >
-                                            {{
-                                                $t(
-                                                    "Excelencia agrícola desde 1933"
-                                                )
-                                            }}
-                                        </h2>
-
-                                        <p
-                                            class="lightFont visible mx-0 mt-3 mb-0 text-xl leading-relaxed text-left text-slate-200"
+                                            class="lightFont visible mx-0 mt-3 mb-0 lg:text-2xl text-xl leading-relaxed text-left text-slate-200 pr-8"
                                         >
                                             {{
                                                 $t(
@@ -78,17 +53,3 @@
         </div>
     </section>
 </template>
-
-<style>
-#hero {
-    width: 100%;
-    height: 100vh;
-    overflow: hidden;
-    position: relative;
-}
-@media (max-height: 500px) {
-    #hero {
-        height: 150vh;
-    }
-}
-</style>
