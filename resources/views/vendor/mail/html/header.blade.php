074a4da0c13828ec29274636312e61afecc8915c
Christian Rivas <99852554+Arreagaa@users.noreply.github.com>
2023-06-07 14:30:29 -0600

    fix

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url}}">
-    <img src="{{ asset('/assets/Logos/LogoFooter.png') }}">
+    <img src="{{ asset('/assets/1-Principal/LogoMail.png') }}">
 </a>
 </td>
 </tr>
